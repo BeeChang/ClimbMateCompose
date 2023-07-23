7f2d665722b53e9e68d2169bead8e1123fab562c
beechang <dbsckd92@naver.com>
2023-07-24 00:47:13 +0900

    Edit default theme -> custom theme

--- app/src/main/java/example/beechang/climbmatecompose/MainActivity.kt
+++ app/src/main/java/example/beechang/climbmatecompose/MainActivity.kt
@@ -7,40 +7,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import example.beechang.climbmatecompose.ui.theme.ClimbMateComposeTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ClimbMateComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            val widthSizeClass = calculateWindowSizeClass(this).widthSizeClass
+            ClimbMateApp(widthSizeClass)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ClimbMateComposeTheme {
-        Greeting("Android")
-    }
-}
