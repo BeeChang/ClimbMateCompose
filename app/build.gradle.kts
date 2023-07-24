510b5b7ce02dc2d02825a497b9d0e9a46c97effd
beechang <dbsckd92@naver.com>
2023-07-24 23:50:12 +0900

    ADD accompaist pager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.accompanist.pager)
 
 //    navigation
     implementation(libs.androidx.navigation.compose)
