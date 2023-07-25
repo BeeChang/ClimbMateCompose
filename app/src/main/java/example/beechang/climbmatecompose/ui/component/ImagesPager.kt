a425da79a674521348af52ab3add9b15f6a07a24
beechang <dbsckd92@naver.com>
2023-07-25 17:26:32 +0900

    ADD banner images pager at home

---
+++ app/src/main/java/example/beechang/climbmatecompose/ui/component/ImagesPager.kt
@@ -0,0 +1,155 @@
+package example.beechang.climbmatecompose.ui.component
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import example.beechang.climbmatecompose.R
+import example.beechang.climbmatecompose.ui.component.PagerDotsIndicator
+import example.beechang.climbmatecompose.ui.theme.ClimbMateComposeTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+        /**
+         * @param imageUrlList = list type are only 2 kinds of int(drawble) ,  string(network link , file path)
+         */
+fun ImagesPager(
+    imageUrlList: List<Any> ,
+    autoSlide: Boolean = true,
+    delayTime: Long = 2000,
+    aspectRatio: Float = 16f / 9f,
+    modifier: Modifier = Modifier,
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    onImageClick: (Any) -> Unit = {},
+    hasDotIndicator: Boolean,
+    dotIndicator: @Composable (selectedIndex: Int, size: Int) -> Unit = { _, _ -> }
+) {
+    val pagerState: PagerState = rememberPagerState()
+    val configuration = LocalConfiguration.current
+    val height = configuration.screenWidthDp.dp / aspectRatio
+    
+    if (autoSlide) {
+        LaunchedEffect(pagerState) {
+            while (true) {
+                delay(delayTime)
+                coroutineScope.launch {
+                    val nextPage = (pagerState.currentPage + 1) % imageUrlList.size
+                    pagerState.animateScrollToPage(nextPage)
+                }
+            }
+        }
+    }
+
+    Box(
+        modifier
+            .fillMaxWidth()
+            .height(height)
+    ) {
+        HorizontalPager(
+            pageCount = imageUrlList.size,
+            state = pagerState,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxSize(),
+        ) { page ->
+            when (val imagePath = imageUrlList[page]) {
+                is Int, String -> {
+                    AsyncImage(
+                        model = imagePath,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .clickable { onImageClick(imageUrlList[page]) }
+                            .fillMaxSize()
+                    )
+                }
+                else -> throw IllegalArgumentException("Invalid type for imageUrl")
+            }
+        }
+        if (hasDotIndicator) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.Bottom,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+            ) {
+                dotIndicator(pagerState.currentPage, imageUrlList.size)
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview
+@Composable
+fun ImagesPagerPreview() {
+    val pagerState: PagerState = rememberPagerState()
+    val configuration = LocalConfiguration.current
+    val aspectRatio = 16f / 9f
+    val height = configuration.screenWidthDp.dp / (aspectRatio)
+
+    ClimbMateComposeTheme {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .height(height)
+        ) {
+            HorizontalPager(
+                pageCount = 1,
+                state = pagerState,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .aspectRatio(aspectRatio)
+            ) { page ->
+                AsyncImage(
+                    model = R.drawable.banner_a,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize(),
+                    placeholder = painterResource(R.drawable.banner_a),
+
+                    )
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.Bottom,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+            ) {
+                PagerDotsIndicator(
+                    totalDots = 5,
+                    selectedIndex = 0,
+                )
+            }
+        }
+    }
+}
+
