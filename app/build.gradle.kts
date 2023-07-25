83e27512820d42ffd6b689d0c7e5961b66adc192
beechang <dbsckd92@naver.com>
2023-07-25 16:43:21 +0900

    ADD coil ( image loader library)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.accompanist.pager)
+    implementation(libs.coil.kt.compose)
+
 
 //    navigation
     implementation(libs.androidx.navigation.compose)
