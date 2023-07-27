51a1fe63c174909b531df3b865fd9ab6ff16cbb9
beechang <dbsckd92@naver.com>
2023-07-27 12:51:30 +0900

    ADD pager state and inject the parameter to imagepager for the bottom tab can be maintained a pager state for changes

--- app/src/main/java/example/beechang/climbmatecompose/ui/home/HomeScreen.kt
+++ app/src/main/java/example/beechang/climbmatecompose/ui/home/HomeScreen.kt
@@ -2,17 +2,25 @@ package example.beechang.climbmatecompose.ui.home
 
 import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import example.beechang.climbmatecompose.R
@@ -28,6 +36,8 @@ fun HomeScreen(
     isDarkTheme: Boolean = isSystemInDarkTheme(),
     coroutineScope: CoroutineScope = rememberCoroutineScope(),
 ) {
+    val pagerState = rememberPagerState()
+
     Box(
         modifier = modifier.fillMaxSize()
     ) {
@@ -38,6 +48,7 @@ fun HomeScreen(
                 .padding(horizontal = 16.dp) //HomeHeaderBar height = 64 , add margin top = 8
         ) {
             ImagesPager(
+                pagerState = pagerState ,
                 modifier = Modifier.clip(MaterialTheme.shapes.small),
                 imageUrlList = listOf(
                     R.drawable.banner_a,
