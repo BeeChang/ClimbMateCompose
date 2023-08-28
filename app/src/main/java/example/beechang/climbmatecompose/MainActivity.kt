7e6a91b632fefff9b95eb8e60ae46f84aaec0927
beechang <dbsckd92@naver.com>
2023-08-28 22:49:03 +0900

    ADD hilt android entry point

--- app/src/main/java/example/beechang/climbmatecompose/MainActivity.kt
+++ app/src/main/java/example/beechang/climbmatecompose/MainActivity.kt
@@ -3,11 +3,17 @@ package example.beechang.climbmatecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import example.beechang.climbmatecompose.ui.theme.ClimbMateComposeTheme
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
