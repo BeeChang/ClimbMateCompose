106177376e4c565149f3407e18e5d4c4e98bbf70
beechang <dbsckd92@naver.com>
2023-08-28 22:48:01 +0900

    ADD permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,8 +80,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.accompanist.pager)
-    implementation(libs.accompanist.permissions)
-
     implementation(libs.coil.kt.compose)
 
 //    timber
